c2cf973631078957b81f6fc856ae25f57dc0ef26
adebayo abiodun <106762233+leykhan16@users.noreply.github.com>
2023-03-19 18:26:45 +0100

    voting system

---
+++ task2.php
@@ -0,0 +1,30 @@
+<?php
+//validation systeem for a single voting system;
+
+$voters_age = "18 and above";
+$voters_PVC = "True";
+$voters_ward = "020";
+
+// if $voters_age >= 18 and above {
+//    echo "voter is eligible";
+//}
+// else {
+    // echo " not elligible";
+//};
+
+// if ($voters_PVC = True){
+    // echo "elligible"
+//};
+// else {
+    // echo "not elligible"
+//};
+
+// if ($votres_ward = 020){
+ //   echo "valid";{
+        //else {($voters_ward !== 020)
+            // echo " not valid"
+        //}
+      //  }  
+   // }
+//}
+?>
